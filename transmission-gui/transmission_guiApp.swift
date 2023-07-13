0fa19e816274bb7fe8de3e4c6162f9c1da1b055a
pm <pm@101media.ru>
2023-07-13 16:20:42 +0300

    Initial Commit

---
+++ transmission-gui/transmission_guiApp.swift
@@ -0,0 +1,17 @@
+//
+//  transmission_guiApp.swift
+//  transmission-gui
+//
+//  Created by Михаил Погребников on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct transmission_guiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
